de17be2aa83c9a6b414bbe866c7a6f0f4551d5a6
Jeremy Schlatter <jeremy.schlatter@gmail.com>
2016-12-03 04:26:52 -0800

    install Go and libjpeg-turbo at build time

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,1 +1,13 @@
+GOOS=$(uname | tr '[:upper:]' '[:lower:]')
+if [ $ARCH = '64' ]; then
+    GOARCH=amd64
+else
+    GOARCH=386
+fi
+FN=go1.7.4.$GOOS-$GOARCH.tar.gz
+wget https://storage.googleapis.com/golang/$FN
+tar xvf $FN
+export GOROOT=$(pwd)/go
+export PATH=$GOROOT/bin:$PATH
+
 pip install .
